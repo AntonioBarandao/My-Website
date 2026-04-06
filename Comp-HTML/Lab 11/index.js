2ed6d8a9c6522ad2945f6f9dfc74ab913d22a2ba
Antonio Barandao <techmaster2312@gmail.com>
2026-04-06 13:36:45 -0400

    Lab12

--- Comp-HTML/Lab 11/index.js
+++ Comp-HTML/Lab 11/index.js
@@ -5,12 +5,9 @@ const studentRoutes = require('./routes/studentRoutes');
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/student_lab', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect('mongodb://127.0.0.1:27017/student_lab')
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
-
 app.use('/api/students', studentRoutes);
 
 const PORT = 5000;
